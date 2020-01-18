0d2591d7ed32c61a2c2be8997ae49df60251a30b
Chatdanai Phakaket <zchatdanai@gmail.com>
2020-01-19 00:48:02 +0700

    Change attribute of header and data - close #6

--- myBlockchain/block/__init__.py
+++ myBlockchain/block/__init__.py
@@ -8,12 +8,14 @@ from security import Security
 
 class Block():
     def __init__(self):
-        self.__index = 0
-        self.__timestamp = None
-        self.__authorize = None
-        self.__data = {
+        self.__index = 0,
+        self.__header = {
             "prevHash": None,
             "blockHash": None,
+            "timestamp": None,
+            "authorize": None
+        }
+        self.__data = {
             "gpsPosition": None
         }
 
@@ -24,12 +26,12 @@ class Block():
         jsonData = json.dumps(data, sort_keys=True).encode('utf-8')
         self.__data = base58.b58encode(jsonData)
 
-
     def setValueBlock(self, data=None):
         self.__index = data["index"]
-        self.__timestamp = datetime.datetime.now()
+        self.__header["timestamp"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.__data["gpsPosition"] = base58.b58encode(json.dumps(data["gpsPosition"]).encode('utf-8')).decode()
-        self.__authorize = data["authorize"]
+        self.__header["authorize"] = data["authorize"]
+
         if data["lastBlock"] is None:
             self.__generatePrevHash()
         else:
@@ -39,34 +41,49 @@ class Block():
         self.__data = base58.b58encode(jsonData)
 
     def __generateBlockHash(self):
-        self.__data["blockHash"] = hashlib.sha512(json.dumps(self.__data, sort_keys=True).encode('utf-8')).hexdigest()
+        hashData = {
+            "index": self.__index,
+            "header": self.__header,
+            "data": self.__data
+        }
+        self.__header["blockHash"] = hashlib.sha512(json.dumps(hashData, sort_keys=True).encode('utf-8')).hexdigest()
 
     def __generatePrevHash(self, lastBlockData=None):
         if lastBlockData is None:
-            self.__data["prevHash"] = hashlib.sha512(
+            self.__header["prevHash"] = hashlib.sha512(
                 hashlib.sha512(json.dumps(lastBlockData, sort_keys=True).encode('utf-8')).hexdigest().encode(
                     'utf-8')).hexdigest()
         else:
-            self.__data["prevHash"] = hashlib.sha512(
+            self.__header["prevHash"] = hashlib.sha512(
                 json.dumps(lastBlockData, sort_keys=True).encode('utf-8')).hexdigest()
 
     def getIndex(self):
         return self.__index
 
-    def getTimestamp(self):
-        return self.__timestamp
-
     def getData(self):
         temp = base58.b58decode(self.__data)
         jsonData = json.loads(temp)
-        return jsonData
+        retData = {
+            "index": self.__index,
+            "header": self.__header,
+            "data": jsonData
+        }
 
-    def getBlockData(self,scriptKey=None):
-        if Security.verifySignature(scriptKey=scriptKey,publicKey=self.__authorize):
+
+        return retData
+
+    def getBlockData(self, scriptKey=None):
+        if Security.verifySignature(scriptKey=scriptKey, publicKey=self.__header["authorize"]):
             temp = base58.b58decode(copy.deepcopy(self.__data))
             jsonData = json.loads(temp)
             jsonData["gpsPosition"] = base58.b58decode(jsonData["gpsPosition"]).decode()
 
-            return jsonData
+            retData = {
+                "index": self.__index,
+                "header": self.__header,
+                "data": jsonData
+            }
+
+            return retData
 
         return ""
